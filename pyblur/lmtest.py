a704b77a8b1bf120673571122227d057994acd44
qingshan412 <qingshan412@hotmail.com>
2018-04-04 15:57:41 -0400

    tset

--- pyblur/lmtest.py
+++ pyblur/lmtest.py
@@ -1,5 +1,5 @@
 from PIL import Image
 from LinearMotionBlur import *
 
-imgL = Image.open('../BluredImages/orig.png').convert('L')
+imgL = Image.open('../../BluredImages/orig.png').convert('L')
 blurred = LinearMotionBlur(imgL, 11, 179, 'full')
