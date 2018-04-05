69c23aaf8a316dee299720ee4459bf6037e8891e
qingshan412 <qingshan412@hotmail.com>
2018-04-05 15:17:38 -0400

    Update lmtest.py

--- pyblur/lmtest.py
+++ pyblur/lmtest.py
@@ -1,4 +1,9 @@
 from PIL import Image
+from PIL import ImageFilter
+from PIL.ImageFilter import (
+    BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+    EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN
+    )
 from LinearMotionBlur import *
 
 imgL = Image.open('../../BluredImages/orig.png').convert('L')
