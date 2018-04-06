3a2aa4a22c6ed5bf9f60f9cabd7ceea8d4dddaab
qingshan412 <qingshan412@hotmail.com>
2018-04-05 22:08:35 -0400

    test

--- pyblur/LinearMotionBlur.py
+++ pyblur/LinearMotionBlur.py
@@ -26,8 +26,8 @@ def DirectMove(img, dim):
     half = int(dim/2)
     # im_t1 = img_np[half:640,:]
     new_im_np = np.zeros(img_np.shape,dtype="uint8")
-    new_im_np[:,0:640-half] = img_np[:,0:640-half]*0.5
-    new_im_np[:,half:640] += img_np[:,half:640]*0.5
+    new_im_np[:,0:640-half] = (img_np[:,0:640-half]/2)
+    new_im_np[:,half:640] += (img_np[:,0:640-half]/2)#(img_np[:,half:640]/2)
     #new_im_np[new_im_np > 255] = 255
     new_img = Image.fromarray(new_im_np,dtype="uint8")
 
